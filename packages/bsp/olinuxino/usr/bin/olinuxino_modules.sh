dd71648aa0aabf972c6c1d21458a0318ef3103a9
Stefan Mavrodiev <stefan@olimex.com>
2019-01-17 15:53:25 +0200

    Update olinuxino-modules service

--- packages/bsp/olinuxino/usr/bin/olinuxino_modules.sh
+++ packages/bsp/olinuxino/usr/bin/olinuxino_modules.sh
@@ -4,37 +4,28 @@ ID="/proc/device-tree/id"
 
 case "$1" in
 	start)
+
+		# Load modules depending on board type
 		case "$ID" in
 			# A20-SOM204-1G
 			8991)
-				modprobe gpio-ir-tx r8723bs
-				exit $?
+				modprobe gpio-ir-tx r8723bs || exit $?
 				;;
 
 			# A20-SOM204-1Gs16Me16G-MC
 			8958)
-				modprobe gpio-ir-tx r8723bs smsc
-				exit $?
+				modprobe gpio-ir-tx r8723bs smsc || exit $?
 				;;
 		esac
+
+		# Load modules depending on lcd type
+		if [[ -e '/proc/device-tree/soc@1c00000/rtp@1c25000/allwinner,ts-attached' ]]; then
+			rmmod sun4i_gpadc_iio sun4i_gpadc sun4i_ts 2>/dev/null
+			modprobe sun4i_ts || exit $?
+		fi
 	;;
 
-	stop)
-		case "$ID" in
-			# A20-SOM204-1G
-			8991)
-				rmmod gpio-ir-tx r8723bs
-				exit $?
-				;;
-			# A20-SOM204-1Gs16Me16G-MC
-			8958)
-				rmmod gpio-ir-tx r8723bs smsc
-				exit $?
-				;;
-		esac
-		;;
-
 	*)
-		echo "Usage: $0 {start|stop}"
+		echo "Usage: $0 {start}"
 		;;
 esac
