59bc095be98d385f58b9a412924d79877cd2362b
Stefan Mavrodiev <stefan@olimex.com>
2019-01-17 14:48:48 +0200

    Load SMSC module for A20-SOM204-xxx-MC

--- packages/bsp/olinuxino/usr/bin/olinuxino_modules.sh
+++ packages/bsp/olinuxino/usr/bin/olinuxino_modules.sh
@@ -5,23 +5,32 @@ ID="/proc/device-tree/id"
 case "$1" in
 	start)
 		case "$ID" in
-			# A20-SOM204-1G | A20-SOM204-1Gs16Me16G-MC
-			8991|\
-			8958)
+			# A20-SOM204-1G
+			8991)
 				modprobe gpio-ir-tx r8723bs
 				exit $?
-			;;
+				;;
+
+			# A20-SOM204-1Gs16Me16G-MC
+			8958)
+				modprobe gpio-ir-tx r8723bs smsc
+				exit $?
+				;;
 		esac
 	;;
 
 	stop)
 		case "$ID" in
-			# A20-SOM204-1G | A20-SOM204-1Gs16Me16G-MC
-			8991|\
-			8958)
+			# A20-SOM204-1G
+			8991)
 				rmmod gpio-ir-tx r8723bs
 				exit $?
-			;;
+				;;
+			# A20-SOM204-1Gs16Me16G-MC
+			8958)
+				rmmod gpio-ir-tx r8723bs smsc
+				exit $?
+				;;
 		esac
 		;;
 
